908a7a6232abde198a148c0a695437d2fc1f356d
llf0029 <llf0029@users.noreply.github.com>
2016-09-11 15:14:32 +1200

    Added tests for statistics calculator
    Added testing code for get_min_height/weight, get_max_height/weight and get_avg_height/weight methods.

--- docTestsRunner.py
+++ docTestsRunner.py
@@ -1,6 +1,7 @@
 def test():
     import doctest
-    doctest.testfile("test_createPokemon.txt", verbose=1)
+    # doctest.testfile("test_createPokemon.txt", verbose=1)
+    doctest.testfile("test_statisticsCalculator.txt", verbose=1)
 
 
 if __name__ == "__main__":
