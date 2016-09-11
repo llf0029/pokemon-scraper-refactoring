a691b2de05604c35a2fe1a4e47a366754cca9a59
llf0029 <llf0029@users.noreply.github.com>
2016-09-11 15:36:52 +1200

    Extracted calculate_min functionality
    Extracted duplicated code from get_min_height() and get_min_weight() and moved it into a generic calculate_min() method.

--- StatisticCalculator.py
+++ StatisticCalculator.py
@@ -6,13 +6,10 @@
 class StatisticCalculator:
 
     def get_min_weight(self, pokedex):
-        min_weight = 1000000000
-        lightest_pokemon = ""
-        for name in pokedex:
-            if pokedex[name].get_weight() < min_weight:
-                min_weight = pokedex[name].get_weight()
-                lightest_pokemon = name
-        return lightest_pokemon
+        items = {}
+        for key in pokedex:
+            items[key] = pokedex[key].get_weight()
+        return self.calculate_min(items)
 
     def get_max_weight(self, pokedex):
         max_weight = -1
@@ -30,13 +27,10 @@ class StatisticCalculator:
         return self.calculate_avg(values)
 
     def get_min_height(self, pokedex):
-        min_height = 1000000000
-        shortest_pokemon = ""
-        for name in pokedex:
-            if pokedex[name].get_height() < min_height:
-                min_height = pokedex[name].get_height()
-                shortest_pokemon = name
-        return shortest_pokemon
+        items = {}
+        for key in pokedex:
+            items[key] = pokedex[key].get_height()
+        return self.calculate_min(items)
 
     def get_max_height(self, pokedex):
         max_height = -1
@@ -46,13 +40,23 @@ class StatisticCalculator:
                 max_height = pokedex[name].get_height()
                 tallest_pokemon = name
         return tallest_pokemon
-
+        
     def get_avg_height(self, pokedex):
         values = []
         for key in pokedex:
             values.append(pokedex[key].get_height())
         return self.calculate_avg(values)
     
+    # Extracted from get_min_height / get_min_weight
+    def calculate_min(self, items):
+        min_val = 1000000000
+        min_key = ""
+        for key, value in items.items():
+            if value < min_val:
+                min_val = value
+                min_key = key
+        return min_key
+
     # Extracted from get_avg_height / get_avg_weight
     def calculate_avg(self, values):
         num_of_values = len(values)
