20add727d758bf53354f6b22d01f5447bfbde2e3
llf0029 <llf0029@users.noreply.github.com>
2016-09-17 18:21:36 +1200

    Added tests for view/output
    Added testing code for create_pokemon, get_stats, get_from_web, get_min_weight/height, get_max_weight/height, get_avg_weight/height, and save_data methods.

--- docTestsRunner.py
+++ docTestsRunner.py
@@ -1,8 +1,8 @@
 def test():
     import doctest
     # doctest.testfile("test_createPokemon.txt", verbose=1)
-    doctest.testfile("test_statisticsCalculator.txt", verbose=1)
-
+    # doctest.testfile("test_statisticsCalculator.txt", verbose=1)
+    doctest.testfile("test_cmdView.txt", verbose=1)
 
 if __name__ == "__main__":
     test()
