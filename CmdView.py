93dab83ee8eb9ea4b364d3f0d158dca57761cc34
llf0029 <llf0029@users.noreply.github.com>
2016-09-17 18:38:17 +1200

    Added implementation of View ABC
    Added CmdView, which implements the View abstract base class

---
+++ CmdView.py
@@ -0,0 +1,35 @@
+# Imports
+import View
+
+# Abstract base class
+class CmdView(View.View):
+
+    def display_message(self, message):
+        pass
+
+    def error_not_found(self, pokemon_name):
+        pass
+
+    def display_added_pokemon(self, pokemon):
+        pass
+
+    def display_pokemon_stats(self, pokemon):
+        pass
+
+    def display_weight_min(self, pokemon_name, pokemon_weight):
+        pass
+
+    def display_weight_max(self, pokemon_name, pokemon_weight):
+        pass
+
+    def display_weight_avg(self, pokemon_name, pokemon_weight):
+        pass
+
+    def display_height_min(self, pokemon_name, pokemon_height):
+        pass
+
+    def display_height_max(self, pokemon_name, pokemon_height):
+        pass
+
+    def display_height_avg(self, pokemon_name, pokemon_height):
+        pass
