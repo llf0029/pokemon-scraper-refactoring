db9464b20473b22e24e21e29fc194fc5f41264f0
llf0029 <llf0029@users.noreply.github.com>
2016-09-17 19:48:17 +1200

    PEP8 compliance

--- FileHandler.py
+++ FileHandler.py
@@ -10,10 +10,12 @@ class FileHandler:
     def save(self, pokemon):
         database = shelve.open('pokedexData.db')
         database[pokemon.name] = pokemon
+        database.close()
 
     def load_database(self):
         database = shelve.open('pokedexData.db')
         loaded_pokemon = []
         for key in database:
             loaded_pokemon.append(database[key])
+        database.close()
         return loaded_pokemon
